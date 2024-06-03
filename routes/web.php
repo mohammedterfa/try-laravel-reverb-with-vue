9f003e9629c1cf90b89b510b67baa08c88c3fc40
mohammedterfa <mohammedterfa@gmail.com>
2024-06-03 20:56:45 +0300

    fetching users from database

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,7 +9,16 @@ Route::get('/', function () {
 
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard', [
+        "users" => User::whereNot('id', auth()->id())->get()
+    ]);
 })->middleware(['auth'])->name('dashboard');
+
+
+Route::get('/chat/{friend}', function (User $friend) {
+    return view('chat', [
+        "friend" => $friend
+    ]);
+})->middleware(['auth'])->name('chat');
 
 require __DIR__.'/auth.php';
